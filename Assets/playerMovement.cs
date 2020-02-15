11bb1867f956bd277c616b24e329443549faeddc
Ross Hunter <blue_lizzard@hotmail.com>
2020-02-15 17:20:41 +0000

    Initial commit

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    public Rigidbody body;
+
+    public float forwardForce = 2000f;
+    public float movementForce = 500f;
+    public float jumpForce = 1000f;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // Add forward force to player
+       // body.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("w") ) {
+            body.AddForce(0, 0, forwardForce * Time.deltaTime);
+        };
+
+        if ( Input.GetKey("s") ) {
+            body.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        };
+
+
+        if ( Input.GetKey("d") ) {
+            body.AddForce( movementForce * Time.deltaTime , 0, 0);
+        };
+
+        if ( Input.GetKey("a") ) {
+            body.AddForce( -movementForce * Time.deltaTime , 0, 0);
+        };
+
+        if ( Input.GetKey("space") ) {
+            body.AddForce( 0 , jumpForce * Time.deltaTime , 0);
+        };
+    }
+    
+
+}
