39a612fb253667317c56cd8ac781912999b41343
lt73 <R0sshunt3r2070Super>
2020-02-16 12:59:25 +0000

    better jump and postpro

---
+++ Assets/betterJump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class betterJump : MonoBehaviour
+{
+  public float fallMultiplier = 2.5f;
+  public float lowJumpMultiplyer = 2f;
+
+  Rigidbody rigidBody;
+
+  void Awake() {
+    rigidBody = GetComponent<Rigidbody> ();  
+  }
+
+  void FixedUpdate() {
+      if ( rigidBody.velocity.y < -5 ) {
+          rigidBody.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+      }
+
+      print(rigidBody.velocity.y);
+  }
+}
